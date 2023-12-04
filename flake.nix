9d851082836e7b7b42bd40f56b596cbd8f8a08a8
dndll <donovan@near.org>
2023-12-04 09:45:19 +0000

    build: add nargo to flake

--- flake.nix
+++ flake.nix
@@ -5,13 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    noir-lang.url = "github:noir-lang/noir/v0.19.2";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, noir-lang, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
+        pkgs = import nixpkgs { 
+          inherit system overlays; 
+        };
         rustVersion = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
         rustPlatform = pkgs.makeRustPlatform {
           cargo = rustVersion;
@@ -21,6 +24,12 @@
         stdenv = pkgs.clangStdenv;
         devShell = pkgs.mkShell {
           LIBCLANG_PATH = pkgs.libclang.lib + "/lib/";
+          inputsFrom = [
+            noir-lang
+            # noir_wasm
+            # noirc_abi_wasm
+            # acvm_js
+          ];
 
           nativeBuildInputs = with pkgs; [ 
             bashInteractive
@@ -32,6 +41,11 @@
             llvmPackages_11.bintools 
             llvmPackages_11.libclang 
             protobuf
+
+            yarn
+            nodejs-18_x
+
+            noir-lang.packages.${system}.nargo
 
           ];
           buildInputs = with pkgs; [ 
